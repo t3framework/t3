1b6d4f64a66433fd5f9afe694241db87f07ce271
Khanh Le <khanhlh@joomsolutions.com>
2013-01-23 16:17:55 +0700

    Init the T3 Framework package

---
+++ t3.script.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package      T3
+ *
+ * @author       JoomlArt
+ * @copyright    Copyright (C) 2012-2013. All rights reserved.
+ * @license      http://www.gnu.org/licenses/gpl.html GNU/GPL, see LICENSE.txt
+ */
+
+defined('_JEXEC') or die();
+
+
+class T3InstallerScript
+{
+    /**
+     * Called before any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function preflight($route, JAdapterInstance $adapter)
+    {
+        return true;
+    }
+
+
+    /**
+     * Called after any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function postflight($route, JAdapterInstance $adapter)
+    {
+        return true;
+    }
+}
