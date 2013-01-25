7243f51e192bddbbb5d8454c2fc3ebe3b9e1cf00
simbat <manhnv@joomsolutuions.com>
2013-01-25 17:57:01 +0700

    Update layout for joomla default layout
    - Add layout: default-joomla-25
    - Add layout: default-joomla-30

---
+++ source/tpl_t3_blank/tpls/default-joomla-25.php
@@ -0,0 +1,44 @@
+<?php
+/** 
+ *------------------------------------------------------------------------------
+ * @package   T3 Framework for Joomla!
+ *------------------------------------------------------------------------------
+ * @copyright Copyright (C) 2004-2013 JoomlArt.com. All Rights Reserved.
+ * @license   GNU General Public License; http://www.gnu.org/licenses/gpl.html
+ * @author    JoomlArt, JoomlaBamboo 
+ *            If you want to be come co-authors of this project, please follow 
+ *            our guidelines at http://t3-framework.org/contribute
+ *------------------------------------------------------------------------------
+ */
+
+
+defined('_JEXEC') or die;
+?>
+
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
+  <head>
+    <jdoc:include type="head" />
+    <?php $this->loadBlock ('head') ?>  
+  </head>
+
+  <body>
+
+    <?php $this->loadBlock ('header') ?>
+    
+    <?php $this->loadBlock ('mainnav') ?>
+
+    <?php $this->loadBlock ('spotlight-1') ?>
+
+    <?php $this->loadBlock ('mainbody') ?>
+    
+    <?php $this->loadBlock ('spotlight-2') ?>
+    
+    <?php $this->loadBlock ('navhelper') ?>
+    
+    <?php $this->loadBlock ('footer') ?>
+    
+  </body>
+
+</html>
