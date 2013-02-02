40ce540397a79b8a2d2bb9413dc81f56cd8bfa85
joomlart <hungdv@joomsolutions.com>
2013-02-02 14:19:42 +0700

    Fix throw Error when missing T3 Fix Can't remove empty content column http://pm.joomlart.com/browse/JATCMITIUS-80

--- source/tpl_t3_blank/index.php
+++ source/tpl_t3_blank/index.php
@@ -17,7 +17,14 @@ defined('_JEXEC') or die;
 
 //check if t3 plugin is existed
 if(!defined('T3')){
-	T3::error(JText::_('T3_MISSING_T3_PLUGIN'));
+	if (JError::$legacy) {
+		JError::setErrorHandling(E_ERROR, 'die');
+		JError::raiseError(500, JText::_('T3_MISSING_T3_PLUGIN'));
+		
+		exit;
+	} else {
+		throw new Exception(JText::_('T3_MISSING_T3_PLUGIN'), 500);
+	}
 }
 
 $t3app = T3::getApp($this);
