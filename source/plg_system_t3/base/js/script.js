77efbe7c4a6c2855f0d97b4e917dac730c3d2a9c
joomlart <hungdv@joomsolutions.com>
2013-02-21 17:03:51 +0700

    Fix: Touch menu

--- source/plg_system_t3/base/js/script.js
+++ source/plg_system_t3/base/js/script.js
@@ -12,16 +12,83 @@
  */
 
 !function($){
+	var isTouch = 'ontouchstart' in window && !(/hp-tablet/gi).test(navigator.appVersion);
+	
+	if(isTouch){
+		$.fn.touchmenu = function(){
+			return this.each(function(){	
+				var	itemsel = $(this).has('.mega').length ? 'li.mega' : 'li.parent',
+					jitems = $(this).find(itemsel),
+					reset = function(){
+						$(this).data('noclick', 0);
+					},
+					onTouch = function(e){
+						$(document.body).addClass('hoverable');
+
+						e.stopPropagation();
+						
+						var val = !$(this).data('noclick');
+						// reset all
+						jitems.data('noclick', 0);
+						$(this).data('noclick', val);
+
+						var that =  this;
+						
+						if(val){
+							$(this)
+								.data('rsid', setTimeout($.proxy(reset, this), 500))
+								.parent().parentsUntil('.nav').filter(itemsel).addClass('open');
+						}
+
+						this.focus();
+					},
+					onClick = function(e){
+						e.stopPropagation();
+
+						clearTimeout($(this).data('rsid'));
+
+						if($(this).data('noclick')){
+							e.preventDefault();
+							jitems.removeClass('open');
+							$(this).addClass('open').parentsUntil('.nav').filter(itemsel).addClass('open');
+						} else {
+							var href = $(this).children('a').attr('href');
+							if(href){
+								window.location.href = href;
+							}
+						}
+					};
+				
+				jitems.on('touchstart', onTouch).data('noclick', 0);
+				
+				$(this).on('touchstart', 'li', function(e){
+					e.stopPropagation();
+				}).on('click', 'li', onClick);
+
+				$(document).on('touchstart', function(){
+					jitems.data('noclick', 0);
+					$(document.body).removeClass('hoverable');
+				});
+			});
+		};
+	}
+
+	$('html').addClass(isTouch ? 'touch' : 'no-touch');
+
 	$(document).ready(function(){
 		//remove conflict of mootools more show/hide function of element
 		if(window.MooTools && window.MooTools.More && Element && Element.implement){
 			$('.collapse').each(function(){this.show = null; this.hide = null});
 		}
 
-		$(document.body).on('click', '[data-toggle="dropdown"]' ,function(){
-			if($(this).parent().hasClass('open') && this.href && this.href != '#'){
-				window.location.href = this.href;
-			}
-		});
+		if(isTouch){
+			$('ul.nav').has('.dropdown-menu').touchmenu();
+		} else {
+			$(document.body).on('click', '[data-toggle="dropdown"]' ,function(){
+				if($(this).parent().hasClass('open') && this.href && this.href != '#'){
+					window.location.href = this.href;
+				}
+			});
+		}
 	});
 }(jQuery);
