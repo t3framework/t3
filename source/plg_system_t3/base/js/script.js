52967b79d130d3b411024ef54981e6a98a8d8f45
joomlart <hungdv@joomsolutions.com>
2013-02-22 18:15:24 +0700

    JS Conflict: Fix hide event of bootstrap tooltip

--- source/plg_system_t3/base/js/script.js
+++ source/plg_system_t3/base/js/script.js
@@ -103,7 +103,7 @@
 	$(document).ready(function(){
 		//remove conflict of mootools more show/hide function of element
 		if(window.MooTools && window.MooTools.More && Element && Element.implement){
-			$('.collapse').each(function(){this.show = null; this.hide = null});
+			$('.collapse, .hasTooltip').each(function(){this.show = null; this.hide = null});
 		}
 
 		if(isTouch){
