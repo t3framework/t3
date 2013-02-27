1a0cdd93d4a19e907d3a8d7c16eaeea98e443132
joomlart <hungdv@joomsolutions.com>
2013-02-27 15:58:19 +0700

    T3 Blank: change language keys for templateInfo

--- source/tpl_t3_blank/templateInfo.php
+++ source/tpl_t3_blank/templateInfo.php
@@ -25,18 +25,18 @@ defined('_JEXEC') or die;
 <div class="span8">
   <div class="t3-admin-overview-header">
   	<h2>
-      <?php echo JText::_('T3_BLANK_DESC_1') ?>
-      <small style="display: block;"><?php echo JText::_('T3_BLANK_DESC_2') ?></small>
+      <?php echo JText::_('T3_TPL_DESC_1') ?>
+      <small style="display: block;"><?php echo JText::_('T3_TPL_DESC_2') ?></small>
     </h2>
-    <p><?php echo JText::_('T3_BLANK_DESC_3') ?></p>
+    <p><?php echo JText::_('T3_TPL_DESC_3') ?></p>
   </div>
   <div class="t3-admin-overview-body">
-    <h4><?php echo JText::_('T3_BLANK_DESC_4') ?></h4>
+    <h4><?php echo JText::_('T3_TPL_DESC_4') ?></h4>
     <ul class="t3-admin-overview-features">
-      <li><?php echo JText::_('T3_BLANK_DESC_5') ?></li>
-      <li><?php echo JText::_('T3_BLANK_DESC_6') ?></li>
-      <li><?php echo JText::_('T3_BLANK_DESC_7') ?></li>
-      <li><?php echo JText::_('T3_BLANK_DESC_8') ?></li>
+      <li><?php echo JText::_('T3_TPL_DESC_5') ?></li>
+      <li><?php echo JText::_('T3_TPL_DESC_6') ?></li>
+      <li><?php echo JText::_('T3_TPL_DESC_7') ?></li>
+      <li><?php echo JText::_('T3_TPL_DESC_8') ?></li>
     </ul>
   </div>
 </div>
