0bb93853d41ac4a12ac280314673df5187d5580e
joomlart <hungdv@joomsolutions.com>
2013-03-19 08:42:01 +0700

    T3: Sync default value for off-canvas

--- source/plg_system_t3/includes/core/template.php
+++ source/plg_system_t3/includes/core/template.php
@@ -704,11 +704,10 @@ class T3Template extends ObjectExtendable
 		}
 
 		// add css/js for off-canvas
-		if ($this->getParam('navigation_collapse_offcanvas')) {
+		if ($this->getParam('navigation_collapse_offcanvas', 1)) {
 			$this->addCss ('off-canvas', false);
 			$this->addScript (T3_URL.'/js/off-canvas.js', false);
 		}
-
 	}
 
 	function paramToStyle($style, $paramname = '', $isurl = false){
