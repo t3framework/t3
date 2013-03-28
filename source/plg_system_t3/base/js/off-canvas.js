ff0d587e13e29fd6e99fa45897c81ffc9fdf88ac
Khanh Le <khanhlh@joomsolutions.com>
2013-03-28 10:12:54 +0700

    Improve off-canvas menu for mobile

--- source/plg_system_t3/base/js/off-canvas.js
+++ source/plg_system_t3/base/js/off-canvas.js
@@ -12,35 +12,82 @@
  */
 
 !function($){
-	if (!$.browser.msie || $.browser.version >= 10) {
+	if (!$.browser.msie || $.browser.version >= 9) {
 		$(document).ready(function(){
-			var $btn = $('.btn-navbar');
+			var $btn = $('.btn-navbar'),
+					$nav = null,
+					$fixeditems = null;
 			if (!$btn.length) return ;
-			$($btn.data('target')).clone().appendTo ($('<div class="t3-mainnav" />').appendTo($('<div id="off-canvas-nav"></div>').appendTo($('body'))));
+			$nav = $('<div class="t3-mainnav" />').appendTo($('<div id="off-canvas-nav"></div>').appendTo($('body')));
+			$($btn.data('target')).clone().appendTo ($nav);
 			$('html').addClass ('off-canvas');
 			$('.btn-navbar').click (function(e){
 				var $this = $(this);
 				if ($this.data('off-canvas') == 'show') {
-					$this.data('off-canvas', 'hide');
-					$('html').removeClass ('off-canvas-enabled');
+					hideNav();
 				} else {
-					$this.data('off-canvas', 'show');
-					$('html').addClass ('off-canvas-enabled');
+					showNav();
 				}
 				return false;
 			});
 
-			// hide when click on off-canvas-nav
-			$('#off-canvas-nav').bind ('touchstart click', function (e) {
-				if (e.target == this) {
-					var btn = $('.btn-navbar');
-					if (btn.data('off-canvas') == 'show') {
-						btn.data('off-canvas', 'hide');
-						$('html').removeClass ('off-canvas-enabled');
+
+
+			showNav = function () {
+				var t = $(window).scrollTop(),
+					$mainnav = $('#t3-mainnav');
+				$nav[0].style.top = t+'px';
+				wpfix(1);
+				setTimeout (function(){	
+						$('.btn-navbar').data('off-canvas', 'show');
+						$('html').addClass ('off-canvas-enabled');
+						$(window).bind('scroll touchmove', function () {
+							var t = $(window).scrollTop();
+							if (t < $nav.position().top) $nav[0].style.top = t+'px';
+						});
+						// hide when click on off-canvas-nav
+						$('#off-canvas-nav').bind ('click', function (e) {
+							e.stopPropagation();
+							return false;
+						});
+						$('body').bind ('click', function (e) {
+							e.preventDefault();
+							hideNav();
+							return false;
+						});
+					}, 50);
+				setTimeout (function(){
+					wpfix(2);
+				}, 1000);
+			};
+
+			hideNav = function () {				
+				$(window).unbind('scroll touchmove');
+				$('#off-canvas-nav').unbind ('click');
+				$('body').unbind ('click');
+				$('html').removeClass ('off-canvas-enabled');
+				$('.btn-navbar').data('off-canvas', 'hide');
+				setTimeout (function(){
+				}, 1000);
+			};
+
+			wpfix = function (step) {
+				// check if need fixed
+				if ($fixeditems == -1) return ;// no need to fix
+				if (!$fixeditems) {
+					$fixeditems = $('body').children().filter(function(){ return $(this).css('position') === 'fixed' });
+					if (!$fixeditems.length) {
+						$fixeditems = -1;
+						return ;
 					}
-					return false;
 				}
-			});
+
+				if (step==1) {
+					$fixeditems.css({'position': 'absolute', 'top': $(window).scrollTop()+'px'});
+				} else {
+					$fixeditems.css({'position': '', 'top': ''});
+				}
+			}
 
 		})
 	}
