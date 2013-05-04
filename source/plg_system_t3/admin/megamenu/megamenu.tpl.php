ebb40dd035eef50b90c617f3ecbeb2a9030b779c
joomlart <hungdv@joomsolutions.com>
2013-05-04 10:14:07 +0700

    Megamenu: Correct link to font awesome instead of bootstrap font Fixed: https://github.com/t3framework/t3/issues/86

--- source/plg_system_t3/admin/megamenu/megamenu.tpl.php
+++ source/plg_system_t3/admin/megamenu/megamenu.tpl.php
@@ -78,7 +78,7 @@ $modules = $db->loadObjectList();
           <ul>
             <li>
               <label class="hasTip" title="<?php echo JTexT::_('T3_NAVIGATION_MM_ICON'), '::', JTexT::_('T3_NAVIGATION_MM_ICON_DESC') ?>">
-                <a href="http://twitter.github.com/bootstrap/base-css.html#icons" target="_blank"><i class="icon-search"></i><?php echo JTexT::_('T3_NAVIGATION_MM_ICON') ?></a>
+                <a href="http://fortawesome.github.io/Font-Awesome/#icons-web-app" target="_blank"><i class="icon-search"></i><?php echo JTexT::_('T3_NAVIGATION_MM_ICON') ?></a>
               </label>
               <fieldset class="">
                 <input type="text" class="input-medium toolitem-xicon toolbox-input" name="toolitem-xicon" data-name="xicon" value="" />
