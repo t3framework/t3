7d3f4992a0c69d797e187776566f706995b4665c
joomlart <hungdv@joomsolutions.com>
2013-07-22 13:32:09 +0700

    Megamenu: Fix error on browserNav Fix https://github.com/t3framework/t3/issues/124

--- source/plg_system_t3/includes/menu/megamenu.tpl.php
+++ source/plg_system_t3/includes/menu/megamenu.tpl.php
@@ -219,7 +219,7 @@ class T3MenuMegamenuTpl {
 				break;
 			case 2:
 				// window.open
-				$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$params->get('window_open');
+				$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes';
 				$link = "<a class=\"$class\" href=\"$flink\" onclick=\"window.open(this.href,'targetWindow','$options');return false;\" $title $dropdown>$icon$linktype$caret$caption</a>";
 				break;
 		endswitch;
