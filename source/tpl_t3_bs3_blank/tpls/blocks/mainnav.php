b5ee14b357013c778c9bb8688950708d5e59d4ae
Hieu Nguyen <NNTH@NNTH-MACBOOK.local>
2013-10-17 14:05:03 +0700

    T3 Blank BS3

--- source/tpl_t3_bs3_blank/tpls/blocks/mainnav.php
+++ source/tpl_t3_bs3_blank/tpls/blocks/mainnav.php
@@ -9,8 +9,8 @@ defined('_JEXEC') or die;
 ?>
 
 <!-- MAIN NAVIGATION -->
-<nav id="t3-mainnav" class="wrap t3-mainnav navbar-collapse-fixed-top">
-	<div class="container navbar">
+<nav id="t3-mainnav" class="wrap t3-mainnav navbar navbar-default">
+	<div class="container">
 
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".t3-navbar">
@@ -19,13 +19,14 @@ defined('_JEXEC') or die;
 		</div>
 
 		<div
-			class="t3-navbar navbar-collapse collapse <?php echo $this->getParam('navigation_collapse_showsub', 1) ? ' always-show' : '' ?>">
+			class="t3-navbar collapse navbar-collapse <?php echo $this->getParam('navigation_collapse_showsub', 1) ? ' always-show' : '' ?>">
 			<?php if ($this->getParam('navigation_type') == 'megamenu') : ?>
 				<jdoc:include type="megamenu" />
 			<?php else : ?>
 				<jdoc:include type="modules" name="<?php $this->_p('mainnav') ?>" style="raw" />
 			<?php endif ?>
 		</div>
+
 	</div>
 </nav>
 <!-- //MAIN NAVIGATION -->
