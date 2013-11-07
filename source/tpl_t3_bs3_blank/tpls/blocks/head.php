6d749f709e426febe9afac0effff7c5528934631
joomlart <hungdv@joomsolutions.com>
2013-11-07 10:07:37 +0700

    T3BS3 Blank: Update font-awesome

--- source/tpl_t3_bs3_blank/tpls/blocks/head.php
+++ source/tpl_t3_bs3_blank/tpls/blocks/head.php
@@ -11,6 +11,24 @@ defined('_JEXEC') or die;
 <!-- META FOR IOS & HANDHELD -->
 <?php if ($this->getParam('responsive', 1)): ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	<style type="text/stylesheet">
+		@-webkit-viewport   { width: device-width; }
+		@-moz-viewport      { width: device-width; }
+		@-ms-viewport       { width: device-width; }
+		@-o-viewport        { width: device-width; }
+		@viewport           { width: device-width; }
+	</style>
+	<script type="text/javascript">
+		//<![CDATA[
+		if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+			var msViewportStyle = document.createElement("style");
+			msViewportStyle.appendChild(
+				document.createTextNode("@-ms-viewport{width:auto!important}")
+			);
+			document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+		}
+		//]]>
+	</script>
 <?php endif ?>
 <meta name="HandheldFriendly" content="true"/>
 <meta name="apple-mobile-web-app-capable" content="YES"/>
