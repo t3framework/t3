405b755254f1bcbfa46442242b96e0de31c25faa
Khanh Le <khanhlh@gmail.com>
2013-11-15 16:38:45 +0700

    New off-canvas menu on Bootstrap 3 base
    Refactor the off-canvas menu and apply more animation effect

---
+++ source/plg_system_t3/base-bs3/tpls/blocks/head-offcanvas.php
@@ -0,0 +1,30 @@
+<?php
+/** 
+ *------------------------------------------------------------------------------
+ * @package       T3 Framework for Joomla!
+ *------------------------------------------------------------------------------
+ * @copyright     Copyright (C) 2004-2013 JoomlArt.com. All Rights Reserved.
+ * @license       GNU General Public License version 2 or later; see LICENSE.txt
+ * @authors       JoomlArt, JoomlaBamboo, (contribute to this project at github 
+ *                & Google group to become co-author)
+ * @Google group: https://groups.google.com/forum/#!forum/t3fw
+ * @Link:         http://t3-framework.org 
+ *------------------------------------------------------------------------------
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+?>
+
+<?php
+$this->addCss('off-canvas', false);
+$this->addScript(T3_URL . '/js/off-canvas.js');
+?>
+
+<!-- OFF-CANVAS NAVIGATION -->
+<script type="text/javascript">
+	jQuery(document).ready(function ($) {
+		new OffcanvasMenu($, {style: '<?php echo $this->getParam('navigation_collapse_offcanvas_effect', 'st-effect-4') ?>'});
+	});
+</script>
+<!-- //OFF-CANVAS NAVIGATION -->
