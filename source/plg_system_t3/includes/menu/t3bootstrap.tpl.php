acb10b0a740a0223e871457ea37d7be155f6cdf1
joomlart <hungdv@joomsolutions.com>
2013-11-21 17:08:30 +0700

    Bootstrap menu: Correct the markup of Bootstrap 3

--- source/plg_system_t3/includes/menu/t3bootstrap.tpl.php
+++ source/plg_system_t3/includes/menu/t3bootstrap.tpl.php
@@ -105,8 +105,21 @@ class T3BootstrapTpl
 	public static function component($item)
 	{
 		// Note. It is important to remove spaces between elements.
-		$class = $item->anchor_css ? 'class="' . $item->anchor_css . '" ' : '';
-		$title = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+		$class    = $item->anchor_css ? $item->anchor_css : '';
+		$title    = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+		$caret    = '';
+		$dropdown = '';
+
+		if($item->deeper && $item->level < 2){
+			$class    .= ' dropdown-toggle';
+			$dropdown  = ' data-toggle="dropdown"';
+			$caret     = '<b class="caret"></b>';
+		}
+
+		if(!empty($class)){
+			$class = 'class="'. trim($class) .'" ';
+		}
+
 		if ($item->menu_image) {
 			$item->params->get('menu_text', 1) ?
 				$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
@@ -119,19 +132,19 @@ class T3BootstrapTpl
 			default:
 			case 0:
 				?>
-				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 			case 1:
 				// _blank
 				?>
-				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" target="_blank" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 			case 2:
 				// window.open
 				?>
-				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes');return false;" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 		endswitch;
@@ -157,7 +170,7 @@ class T3BootstrapTpl
 		}
 
 		?>
-		<span class="separator"<?php echo $title; ?>><?php echo $linktype; ?></span>
+		<span class="separator" <?php echo $title; ?>><?php echo $linktype; ?></span>
 		<?php
 	}
 
@@ -165,8 +178,21 @@ class T3BootstrapTpl
 	{
 
 		// Note. It is important to remove spaces between elements.
-		$class = $item->anchor_css ? 'class="' . $item->anchor_css . '" ' : '';
-		$title = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+		$class    = $item->anchor_css ? $item->anchor_css : '';
+		$title    = $item->anchor_title ? 'title="' . $item->anchor_title . '" ' : '';
+		$caret    = '';
+		$dropdown = '';
+
+		if($item->deeper && $item->level < 2){
+			$class    .= ' dropdown-toggle';
+			$dropdown  = ' data-toggle="dropdown"';
+			$caret     = '<b class="caret"></b>';
+		}
+
+		if(!empty($class)){
+			$class = 'class="'. trim($class) .'" ';
+		}
+
 		if ($item->menu_image) {
 			$item->params->get('menu_text', 1) ?
 				$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
@@ -181,20 +207,20 @@ class T3BootstrapTpl
 			default:
 			case 0:
 				?>
-				<a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 			case 1:
 				// _blank
 				?>
-				<a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 			case 2:
 				// window.open
 				$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
 				?>
-				<a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options; ?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a>
+				<a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options; ?>');return false;" <?php echo $title, $dropdown; ?>><?php echo $linktype, $caret; ?></a>
 				<?php
 				break;
 		endswitch;
