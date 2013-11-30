25b09e7cc54e4c45fdb45ca99ce12c37a078ed36
Hieu Nguyen <hieunn@joomsolutions.com>
2013-11-30 15:24:25 +0700

    DARK THEME
    - Dark Theme
    - Fix Megamenu dropdown-header

--- source/tpl_t3_bs3_blank/tpls/blocks/footer.php
+++ source/tpl_t3_bs3_blank/tpls/blocks/footer.php
@@ -33,7 +33,7 @@ defined('_JEXEC') or die;
 				</div>
 				<?php if ($this->getParam('t3-rmvlogo', 1)): ?>
 					<div class="col-md-4 poweredby">
-						<a class="t3-logo t3-logo-dark" href="http://t3-framework.org" title="Powered By T3 Framework"
+						<a class="t3-logo t3-logo-color" href="http://t3-framework.org" title="Powered By T3 Framework"
 						   target="_blank" rel="nofollow">Powered by <strong>T3 Framework</strong></a>
 					</div>
 				<?php endif; ?>
