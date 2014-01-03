c6d937c8e817ebdc8fa186122276259ce9859f36
joomlart <hungdv@joomsolutions.com>
2014-01-03 14:12:28 +0700

    Fix com_content featured grid

--- source/plg_system_t3/base-bs3/html/com_content/featured/default.php
+++ source/plg_system_t3/base-bs3/html/com_content/featured/default.php
@@ -58,7 +58,7 @@ JHtml::_('behavior.caption');
 
 		<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row; ?> row-fluid">
 		<?php endif; ?>
-			<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> span<?php echo round((12 / $this->columns));?>">
+			<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> col-sm-<?php echo round((12 / $this->columns));?>">
 			<?php
 				$this->item = &$item;
 				echo $this->loadTemplate('item');
