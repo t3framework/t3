d84a258d99d1aa10cb6f1d986bf9e18a213966be
joomlart <hungdv@joomsolutions.com>
2014-02-10 13:51:54 +0700

    Update form-search

--- source/plg_system_t3/base-bs3/html/mod_search/default.php
+++ source/plg_system_t3/base-bs3/html/mod_search/default.php
@@ -9,46 +9,44 @@
 
 defined('_JEXEC') or die;
 ?>
-<form class="form-search" action="<?php echo JRoute::_('index.php');?>" method="post">
-	<div class="search<?php echo $moduleclass_sfx ?>">
+<div class="search<?php echo $moduleclass_sfx ?>">
+	<form action="<?php echo JRoute::_('index.php');?>" method="post" class="form-inline form-search">
 		<?php
-			$output = '<label for="mod-search-searchword">'.$label.'</label><input name="searchword" id="mod-search-searchword" maxlength="'.$maxlength.'"  class="form-control '.$moduleclass_sfx.'" type="text" size="'.$width.'" placeholder="'.$text.'" />';
+			$output = '<label for="mod-search-searchword" class="element-invisible">' . $label . '</label> ';
+			$output .= '<input name="searchword" id="mod-search-searchword" maxlength="' . $maxlength . '"  class="form-control" type="text" size="' . $width . '" placeholder="' . $text . '" />';
 
 			if ($button) :
 				if ($imagebutton) :
-					$button = ' <input type="image" value="' . $button_text . '" class="button' . $moduleclass_sfx.'" src="' . $img . '" onclick="this.form.searchword.focus();"/>';
+					$btn_output = ' <input type="image" value="' . $button_text . '" class="button" src="' . $img . '" onclick="this.form.searchword.focus();"/>';
 				else :
-					$button = ' <button class="button' . $moduleclass_sfx . ' btn btn-primary" onclick="this.form.searchword.focus();">' . $button_text . '</button>';
+					$btn_output = ' <button class="button btn btn-primary" onclick="this.form.searchword.focus();">' . $button_text . '</button>';
 				endif;
-			else :
-				$button = '';
+
+				switch ($button_pos) :
+					case 'top' :
+						$output = $btn_output . '<br />' . $output;
+						break;
+
+					case 'bottom' :
+						$output .= '<br />' . $btn_output;
+						break;
+
+					case 'right' :
+						$output .= $btn_output;
+						break;
+
+					case 'left' :
+					default :
+						$output = $btn_output . $output;
+						break;
+				endswitch;
+
 			endif;
-
-			switch ($button_pos) :
-				case 'top' :
-					$button = $button.'<br />';
-					$output = $button.$output;
-					break;
-
-				case 'bottom' :
-					$button = '<br />'.$button;
-					$output = $output.$button;
-					break;
-
-				case 'right' :
-					$output = $output.$button;
-					break;
-
-				case 'left' :
-				default :
-					$output = $button.$output;
-					break;
-			endswitch;
 
 			echo $output;
 		?>
-	<input type="hidden" name="task" value="search" />
-	<input type="hidden" name="option" value="com_search" />
-	<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>" />
-	</div>
-</form>
+		<input type="hidden" name="task" value="search" />
+		<input type="hidden" name="option" value="com_search" />
+		<input type="hidden" name="Itemid" value="<?php echo $mitemid; ?>" />
+	</form>
+</div>
