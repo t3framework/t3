5944ae39c832f42eb2da6e30d73706f7687624e4
Alvaro Mendoza Rios <admin.leoalv@gmail.com>
2015-01-16 10:22:10 -0500

    Update publish_date.php
    Updated icon Calendar Font Awesome

--- source/plg_system_t3/base-bs3/html/layouts/joomla/content/info_block/publish_date.php
+++ source/plg_system_t3/base-bs3/html/layouts/joomla/content/info_block/publish_date.php
@@ -10,7 +10,7 @@
 defined('JPATH_BASE') or die;
 ?>
 			<dd class="published hasTooltip" title="<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', ''); ?>">
-				<i class="icon-calendar"></i>
+				<i class="fa fa-calendar"></i>
 				<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
 					<?php echo JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3')); ?>
 				</time>
