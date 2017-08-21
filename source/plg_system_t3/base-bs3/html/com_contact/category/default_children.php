fe92584bd909a915e9dba1a8846aa4a7238032f5
Duchh <duchh@joomsolutions.com>
2017-08-21 10:47:42 +0700

    Create a menu type List contacts in a category to see the error:
    - Cant hide Contact filter search field, and Display select field on contact category list page
    - Cant show the contact image
    https://www.joomlart.com/forums/topic/problems-with-displaying-joomla-contacts-page/

--- source/plg_system_t3/base-bs3/html/com_contact/category/default_children.php
+++ source/plg_system_t3/base-bs3/html/com_contact/category/default_children.php
@@ -3,31 +3,31 @@
  * @package     Joomla.Site
  * @subpackage  com_contact
  *
- * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 $class = ' class="first"';
-if (count($this->children[$this->category->id]) > 0 && $this->maxLevel != 0) :
+if ($this->maxLevel != 0 && count($this->children[$this->category->id]) > 0) :
 ?>
-<ul class="list-striped list-condensed list-unstyled">
-	<?php foreach($this->children[$this->category->id] as $id => $child) : ?>
-		<?php
-		if($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
-		if(!isset($this->children[$this->category->id][$id + 1]))
+<ul class="list-striped list-condensed">
+<?php foreach ($this->children[$this->category->id] as $id => $child) : ?>
+	<?php
+	if ($this->params->get('show_empty_categories') || $child->numitems || count($child->getChildren())) :
+		if (!isset($this->children[$this->category->id][$id + 1]))
 		{
 			$class = ' class="last"';
 		}
-		?>
-		<li<?php echo $class; ?>>
-			<?php $class = ''; ?>
+	?>
+	<li<?php echo $class; ?>>
+		<?php $class = ''; ?>
 			<h4 class="item-title">
 				<a href="<?php echo JRoute::_(ContactHelperRoute::getCategoryRoute($child->id)); ?>">
 				<?php echo $this->escape($child->title); ?>
 				</a>
 
-				<?php if ($this->params->get('show_cat_items') == 1) :?>
+				<?php if ($this->params->get('show_cat_items') == 1) : ?>
 					<span class="badge badge-info pull-right" title="<?php echo JText::_('COM_CONTACT_CAT_NUM'); ?>"><?php echo $child->numitems; ?></span>
 				<?php endif; ?>
 			</h4>
@@ -48,8 +48,8 @@ if (count($this->children[$this->category->id]) > 0 && $this->maxLevel != 0) :
 				$this->category = $child->getParent();
 				$this->maxLevel++;
 			endif; ?>
-		</li>
-		<?php endif; ?>
-	<?php endforeach; ?>
+	</li>
+	<?php endif; ?>
+<?php endforeach; ?>
 </ul>
 <?php endif; ?>
