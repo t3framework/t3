f88e00a7dfb87c4fc993e80988a1b3b556f6053c
bacsojin_1102 <bacsojin.1102@gmail.com>
2017-09-01 15:46:03 +0700

    #481 Fix select box on layout edit.

--- source/plg_system_t3/base-bs3/tpls/system/spotlight.php
+++ source/plg_system_t3/base-bs3/tpls/system/spotlight.php
@@ -21,13 +21,14 @@ defined('_JEXEC') or die;
 	$poss = $vars['poss'];
 	$spldata = $vars['spldata'];
 	$default = $vars['default'];
+	$optgroup = $vars['optgroup'];
 ?>
 	<!-- SPOTLIGHT -->
 	<div class="t3-spotlight t3-<?php echo $name, ' ', T3_BASE_ROW_FLUID_PREFIX ?>" <?php echo $spldata ?>>
 		<?php foreach ($poss as $i => $pos): ?>
 		<div class="<?php echo T3_BASE_WIDTH_PREFIX, $default[$i] ?>">
 			<?php if ($this->countModules($pos)) : ?>
-				<jdoc:include type="modules" name="<?php echo $pos ?>" data-original="" style="<?php echo $style ?>" />
+				<jdoc:include type="modules" name="<?php echo $pos ?>" data-original="" data-optgroup="<?php echo $optgroup[$i]; ?>" style="<?php echo $style ?>" />
 				<?php else: ?>
 				&nbsp;
 			<?php endif ?>
