2741fe2331ec6474ac29c1c3b201ce9e9dc2d6e5
trungch <trungch@joomsolutions.com>
2017-11-08 14:31:09 +0700

    Improve style for On / Off icon
    Improve style for On / Off icon

--- source/tpl_t3_bs3_blank/tpls/blocks/off-canvas.php
+++ source/tpl_t3_bs3_blank/tpls/blocks/off-canvas.php
@@ -12,7 +12,9 @@ defined('_JEXEC') or die;
 ?>
 
 <button class="btn btn-primary off-canvas-toggle <?php $this->_c('off-canvas') ?>" type="button" data-pos="left" data-nav="#t3-off-canvas" data-effect="<?php echo $this->getParam('addon_offcanvas_effect', 'off-canvas-effect-4') ?>">
-  <i class="fa fa-bars"></i>
+  <span class="bar-first"></span>
+  <span class="bar-mid"></span>
+  <span class="bar-last"></span>
 </button>
 
 <!-- OFF-CANVAS SIDEBAR -->
