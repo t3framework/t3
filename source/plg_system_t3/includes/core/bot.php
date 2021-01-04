0e3bf5d7c6e18947c8d54a6f5a89579ef365affc
kiendt <kienduong.hust@gmail.com>
2021-01-04 11:27:14 +0700

    fixed load data extra field on frontend edit page

--- source/plg_system_t3/includes/core/bot.php
+++ source/plg_system_t3/includes/core/bot.php
@@ -392,7 +392,10 @@ class T3Bot extends JObject
 					$app   = JFactory::getApplication();
 					$input = $app->input;
 					$fdata = empty($data) ? $input->post->get('jform', array(), 'array') : (is_object($data) ? $data->getProperties() : $data);
-					
+					if (isset($data->attribs) && is_string($data->attribs))
+			      	{
+			      		$data->attribs = json_decode($data->attribs, true);
+			      	}
 					if(!empty($fdata['catid']) && is_array($fdata['catid'])) { // create new
 						$catid = end($fdata['catid']);
 					} else { // edit
