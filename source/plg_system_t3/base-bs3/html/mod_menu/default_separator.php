04049ae3d9060688a685b925f38c31124bcdf782
Kien Duong <kienduong.hust@gmail.com>
2022-03-01 14:03:17 +0700

    fixed get params menu item on Joomla 4

--- source/plg_system_t3/base-bs3/html/mod_menu/default_separator.php
+++ source/plg_system_t3/base-bs3/html/mod_menu/default_separator.php
@@ -8,7 +8,11 @@
  */
 
 defined('_JEXEC') or die;
-
+if(version_compare(JVERSION, '4', 'ge')){
+	$itemParams = $item->getParams();
+}else{
+	$itemParams = $item->params;
+}
 $title      = $item->anchor_title ? ' title="' . $item->anchor_title . '"' : '';
 $anchor_css = $item->anchor_css ?: '';
 
@@ -26,7 +30,7 @@ if ($item->menu_image)
 		$linktype = JHtml::_('image', $item->menu_image, $item->title);
 	}
 
-	if ($item->params->get('menu_text', 1))
+	if ($itemParams->get('menu_text', 1))
 	{
 		$linktype .= '<span class="image-title">' . $item->title . '</span>';
 	}
