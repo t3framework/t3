8f35e86bcfc4e2783709fbfddc285957a5e6c191
Kien Duong <kienduong.hust@gmail.com>
2022-11-04 09:15:05 +0700

    fixed extra fields front-end edit not save

--- source/plg_system_t3/includes/core/bot.php
+++ source/plg_system_t3/includes/core/bot.php
@@ -434,15 +434,10 @@ class T3Bot extends JObject
 		if(isset($data->attribs)){
 			$contentTable = \JTable::getInstance('Content', 'JTable',array());
 			$contentTable->load($data->id);
-			$oldAttribs = json_decode($contentTable->attribs, true);
-			$attribs = json_decode($data->attribs, true);
-			foreach ($attribs as $name => $attrib) {
-				if(!empty($oldAttribs[$name])){
-					$oldAttribs[$name] = $attrib;
-				}
-
-			}
-			$data->attribs = json_encode($oldAttribs);
+			$oldAttribs = new \JRegistry($contentTable->attribs);
+			$attribs = new \JRegistry($data->attribs);
+			$oldAttribs->merge($attribs);
+			$data->attribs = $oldAttribs->toString();
 		}
 	}
 }
